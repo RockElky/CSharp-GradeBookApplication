613a95b19a2351515b752b16b341fd8c742e324e
Erik Oehlke <eaoehlke@impromed.com>
2018-07-21 15:33:18 -0500

    d

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,7 +4,7 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name) : base(name)
+        public StandardGradeBook(string name, bool fault) : base(name)
         {
             Type = GradeBookType.Standard;
         }
